vstorm83/gatein-portal

GTNPORTAL-3365 Exception swallow in UserDaoImpl.persistUserInfo()

webui/portal/src/main/java/org/exoplatform/portal/webui/login/UIResetPassword.java
@@ -125,10 +125,14 @@ public class UIResetPassword extends UIForm {
                     uiApp.addMessage(new ApplicationMessage("UIForgetPassword.msg.user-is-disabled", null));
                     return;
                 } else {
-                    user.setPassword(newpassword);
-                    orgService.getUserHandler().saveUser(user, true);
-                    uiMaskWorkspace.createEvent("Close", Phase.DECODE, request).broadcast();
-                    uiApp.addMessage(new ApplicationMessage("UIResetPassword.msg.change-password-successfully", null));
+                    try {
+                        user.setPassword(newpassword);
+                        orgService.getUserHandler().saveUser(user, true);
+                        uiMaskWorkspace.createEvent("Close", Phase.DECODE, request).broadcast();
+                        uiApp.addMessage(new ApplicationMessage("UIResetPassword.msg.change-password-successfully", null));
+                    } catch (Exception e) {
+                        uiApp.addMessage(new ApplicationMessage("UIResetPassword.msg.change-password-fail", null, ApplicationMessage.ERROR));
+                    }
                 }
             }
             request.addUIComponentToUpdateByAjax(uiMaskWorkspace);

vstorm83/gatein-portal-1

webui/portal/src/main/java/org/exoplatform/portal/webui/login/UIResetPassword.java
@@ -113,10 +113,14 @@ public class UIResetPassword extends UIForm {
             }
 
             if (setPassword) {
-                user_.setPassword(newpassword);
-                orgService.getUserHandler().saveUser(user_, true);
-                uiMaskWorkspace.createEvent("Close", Phase.DECODE, request).broadcast();
-                uiApp.addMessage(new ApplicationMessage("UIResetPassword.msg.change-password-successfully", null));
+                try {
+                    user_.setPassword(newpassword);
+                    orgService.getUserHandler().saveUser(user_, true);
+                    uiMaskWorkspace.createEvent("Close", Phase.DECODE, request).broadcast();
+                    uiApp.addMessage(new ApplicationMessage("UIResetPassword.msg.change-password-successfully", null));
+                } catch (Exception e) {
+                    uiApp.addMessage(new ApplicationMessage("UIResetPassword.msg.change-password-fail", null, ApplicationMessage.ERROR));
+                }
             }
             request.addUIComponentToUpdateByAjax(uiMaskWorkspace);
         }